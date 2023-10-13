9215af2dbff23084b8dd32b63fbfb33fed4198ee
chenjoachim <lllion66666@gmail.com>
2023-10-13 18:54:21 +0800

    hw3 done

--- HW3_ble/ble_scan_connect.py
+++ HW3_ble/ble_scan_connect.py
@@ -1,6 +1,7 @@
 # ble_scan_connect.py:
 from bluepy.btle import Peripheral, UUID
 from bluepy.btle import Scanner, DefaultDelegate
+import binascii
 class ScanDelegate(DefaultDelegate):
     def __init__(self):
         DefaultDelegate.__init__(self)
@@ -9,6 +10,10 @@ class ScanDelegate(DefaultDelegate):
             print ("Discovered device", dev.addr)
         elif isNewData:
             print ("Received new data from", dev.addr)
+    def handleNotification(self, cHandle, data):
+        print(cHandle, data)
+        print ("Notification:", cHandle, "sent data", binascii.b2a_hex(data))
+
 scanner = Scanner().withDelegate(ScanDelegate())
 devices = scanner.scan(10.0)
 n=0
@@ -45,14 +50,26 @@ try:
     print("")
     descs = ch.getDescriptors()
     print(str(descs))
+    real_desc = None
     for desc in descs:
         print(int.from_bytes(desc.read(), byteorder='little'))
         print(desc.uuid)
         if(str(desc.uuid)[0:8] == "00002902"):
+            real_desc = desc
             print("Before writing:", int.from_bytes(desc.read(), byteorder='little'))
             desc.write(0x02.to_bytes(2,'little'))
             print("After writing:", int.from_bytes(desc.read(), byteorder='little'))
                 # print(desc.read())
+    ch.write(0x66.to_bytes(1,'little'))
+    # dev.withDelegate(ScanDelegate(ch.getHandle()))
+    while True:
+        if dev.waitForNotifications(100):
+            # handleNotification() was called
+            print("Notification!")
+            continue
+        # print("After writing:", int.from_bytes(real_desc.read(), byteorder='little'))
+        # print(ch.read())
+        # print("Waiting...")
     
 #
 finally:
